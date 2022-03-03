0e68855a3803f7d10b4daab14bbbc2a840dc7ea2
kedrakk <keaindrak@gmail.com>
2022-03-03 21:34:22 +0630

    first commit

---
+++ android/app/src/main/kotlin/com/myapp/qr_scanner_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.myapp.qr_scanner_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/const/color_const.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyColor {
+  static Color lightBlue = const Color(0xff00B4FF);
+  static Color darkBlue = const Color(0xff002ABA);
+  static Color royalBlue = const Color(0xff0044FF);
+  static Color lightPurple = const Color(0xff9F05C5);
+  static Color darkPurple = const Color(0xff512989);
+  static Color orange = const Color(0xffFFB246);
+  static Color green3 = const Color(0xff2BAE66);
+  static Color green2 = const Color(0xff50C878);
+  static Color green1 = const Color(0xff03C04A);
+  static Color black = const Color(0xff000000);
+  static Color white = const Color(0xffffffff);
+}

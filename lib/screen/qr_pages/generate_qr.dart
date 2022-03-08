f6f2b00f8ef18e0f32e79c905a93c9defe4d01ce
kedrakk <keaindrak@gmail.com>
2022-03-08 21:31:07 +0630

    generate page design

--- lib/screen/qr_pages/generate_qr.dart
+++ lib/screen/qr_pages/generate_qr.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../utils/colors.dart';
 
 class GenerateQRPage extends StatelessWidget {
   const GenerateQRPage({Key? key}) : super(key: key);
@@ -14,8 +17,96 @@ class GenerateQRPage extends StatelessWidget {
           ),
         ),
       ),
-      body: const Center(
-        child: Text("Later"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 20, top: 5),
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: TextFormField(
+                    showCursor: true,
+                    cursorColor: MyColor.white,
+                    style: const TextStyle(color: MyColor.white),
+                    decoration: const InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: MyColor.white,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: MyColor.white,
+                        ),
+                      ),
+                    ),
+                    maxLines: 4,
+                    minLines: 2,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Generate",
+                    style: TextStyle(color: MyColor.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 10,
+            ),
+            width: MediaQuery.of(context).size.width * 1.7 / 2,
+            height: MediaQuery.of(context).size.width * 1.7 / 2,
+            decoration: const BoxDecoration(
+              color: MyColor.grey,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton.icon(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      const RoundedRectangleBorder(
+                        side: BorderSide(color: MyColor.white),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  icon: const Icon(Icons.share),
+                  label: const Text("SHARE NOW"),
+                ),
+                ElevatedButton.icon(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      MyColor.white,
+                    ),
+                    foregroundColor: MaterialStateProperty.all(
+                      Get.theme.scaffoldBackgroundColor,
+                    ),
+                    shape: MaterialStateProperty.all(
+                      const RoundedRectangleBorder(
+                        side: BorderSide(color: MyColor.white),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  icon: const Icon(Icons.check),
+                  label: const Text("SAVE NOW"),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }

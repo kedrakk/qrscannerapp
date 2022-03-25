a8902b7f75de18a6e8cdda595cdaff848942a0a8
kedrakk <keaindrak@gmail.com>
2022-03-25 18:52:43 +0630

    add admob test keys

---
+++ lib/service/ads_helper.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:qr_scanner_app/utils/const.dart';
+
+class AdHelper {
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return bannerAndroidID;
+    } else if (Platform.isIOS) {
+      return bannerIOSID;
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static String get interstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return interstitialAndroidID;
+    } else if (Platform.isIOS) {
+      return interstitialIOSID;
+    } else {
+      throw UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get rewardedAdUnitId {
+    if (Platform.isAndroid) {
+      return rewardAndroidID;
+    } else if (Platform.isIOS) {
+      return rewardIOSID;
+    } else {
+      throw UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get nativeAdUnitId {
+    if (Platform.isAndroid) {
+      return nativeAndroidID;
+    } else if (Platform.isIOS) {
+      return nativeIOSID;
+    } else {
+      throw UnsupportedError("Unsupported platform");
+    }
+  }
+}

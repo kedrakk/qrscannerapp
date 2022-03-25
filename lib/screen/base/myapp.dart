71867c13249b7f4f5f416a523e985a1badd9ca10
kedrakk <keaindrak@gmail.com>
2022-03-25 19:01:49 +0630

    separate binding class

--- lib/screen/base/myapp.dart
+++ lib/screen/base/myapp.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:qr_scanner_app/generated/locales.g.dart';
-import 'package:qr_scanner_app/service/controllers/box_controller.dart';
+import 'package:qr_scanner_app/service/bindings/intitial_binding.dart';
 import 'package:qr_scanner_app/service/controllers/language_service.dart';
 import 'package:qr_scanner_app/service/controllers/theme_service.dart';
 import '../home.dart';
@@ -24,9 +24,10 @@ class MyApp extends StatelessWidget {
       locale: LanguageService().getCurrentLanguage(),
       translationsKeys: AppTranslation.translations,
       fallbackLocale: const Locale('en', 'US'),
-      initialBinding: BindingsBuilder.put(
-        () => BoxController(),
-      ),
+      // initialBinding: BindingsBuilder.put(
+      //   () => BoxController(),
+      // ),
+      initialBinding: RootBinding(),
     );
   }
 }
